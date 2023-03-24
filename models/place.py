c4efc2f60b1f807aaba91967f8f893d7220d094d
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2023-03-24 14:38:51 +0100

    Create place.py

---
+++ models/place.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+<<<<<<< HEAD
+"""This module creates a Place class"""
+
+=======
+"""Defines the Place class."""
+>>>>>>> 8f984e8bc64ff190baab0ef0a90cff0f3aa44787
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+<<<<<<< HEAD
+    """Class for managing place objects"""
+=======
+    """Represent a place.
+    Attributes:
+        city_id (str): The City id.
+        user_id (str): The User id.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms of the place.
+        number_bathrooms (int): The number of bathrooms of the place.
+        max_guest (int): The maximum number of guests of the place.
+        price_by_night (int): The price by night of the place.
+        latitude (float): The latitude of the place.
+        longitude (float): The longitude of the place.
+        amenity_ids (list): A list of Amenity ids.
+    """
+>>>>>>> 8f984e8bc64ff190baab0ef0a90cff0f3aa44787
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
