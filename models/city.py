319c1277800242a64e87656a469c5809226d93ea
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2023-03-24 14:37:44 +0100

    Create city.py

---
+++ models/city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+<<<<<<< HEAD
+"""This module creates a User class"""
+
+=======
+"""Defines the City class."""
+>>>>>>> 8f984e8bc64ff190baab0ef0a90cff0f3aa44787
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+<<<<<<< HEAD
+    """Class for managing city objects"""
+=======
+    """Represent a city.
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
+>>>>>>> 8f984e8bc64ff190baab0ef0a90cff0f3aa44787
+
+    state_id = ""
+    name = ""
