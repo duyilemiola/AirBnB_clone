d1f3ada9602131d6dbba82e8c856be9c2bdd4f61
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2023-03-24 14:21:28 +0100

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+<<<<<<< HEAD
+"""This module creates a Amenity class"""
+
+=======
+"""Defines the Amenity class."""
+>>>>>>> 8f984e8bc64ff190baab0ef0a90cff0f3aa44787
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+<<<<<<< HEAD
+    """Class for managing amenity objects"""
+=======
+    """Represent an amenity.
+    Attributes:
+        name (str): The name of the amenity.
+    """
+>>>>>>> 8f984e8bc64ff190baab0ef0a90cff0f3aa44787
+
+    name = ""
