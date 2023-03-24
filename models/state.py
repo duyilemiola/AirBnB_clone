92980f36b1fd7789755a4cf73272003c8619389f
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2023-03-24 14:40:50 +0100

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+<<<<<<< HEAD
+"""This module creates a User class"""
+
+=======
+"""Defines the State class."""
+>>>>>>> 8f984e8bc64ff190baab0ef0a90cff0f3aa44787
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+<<<<<<< HEAD
+    """Class for managing state objects"""
+=======
+    """Represent a state.
+    Attributes:
+        name (str): The name of the state.
+    """
+>>>>>>> 8f984e8bc64ff190baab0ef0a90cff0f3aa44787
+
+    name = ""
